221adebc70126a8b97504e2b1d0c9d27e26f0cae
masayuki038 <masayuki038@gmail.com>
2013-12-23 07:42:52 +0000

    first commit

---
+++ src/javier_app.erl
@@ -0,0 +1,40 @@
+-module(javier_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ok = bootstrap_cowboy(),
+    javier_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+
+bootstrap_cowboy() ->
+    application:set_env(lager, handlers, [
+        {lager_console_backend, info},
+        {lager_file_backend, [
+            {"error.log", error, 10485760, "$D0", 5},
+            {"console.log", info, 10485760, "$D0", 5}
+        ]}
+    ]),
+    ok = econfig:register_config(javier, ["./javier.ini"], [autoreload]),
+    true = econfig:subscribe(javier),
+    Prefix = helper:get_request_prefix(),
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {Prefix ++ "/", toppage_handler, []},
+            {Prefix ++ "/websocket", websocket_handler, []},
+            {Prefix ++ "/static/[...]", cowboy_static, {priv_dir, javier, <<"static">>}}
+        ]}
+    ]),
+    Port = econfig:get_value(javier, "server", "port"),
+    {ok, _} = cowboy:start_http(sample_http_handler, 100, [{port, list_to_integer(Port)}], [{env, [{dispatch, Dispatch}]}]),
+    ok.
---
+++ src/websocket_handler.erl
@@ -0,0 +1,28 @@
+-module(websocket_handler).
+
+-behaviour(cowboy_websocket_handler).
+-export([init/3]).
+-export([websocket_init/3]).
+-export([websocket_handle/3]).
+-export([websocket_info/3]).
+-export([websocket_terminate/3]).
+
+init({tcp, http}, _Req, _Opts) ->
+    lager:info("init/3"),
+    {upgrade, protocol, cowboy_websocket}.
+
+websocket_init(_TransportName, Req, _Opts) ->
+    lager:info("websocket_init/3"),
+    {ok, Req, undefined_state}.
+ 
+websocket_handle({text, Msg}, Req, State) ->
+    lager:info("websocket_handle/3"),
+    lager:info(Msg).
+
+websocket_info(_Info, Req, State) ->    
+    lager:info("websocket_info/3"),
+    {ok, Req, State}.
+    
+websocket_terminate(_Reason, _Req, _State) -> 
+    lager:info("websocket_terminate/3"),
+    ok.
